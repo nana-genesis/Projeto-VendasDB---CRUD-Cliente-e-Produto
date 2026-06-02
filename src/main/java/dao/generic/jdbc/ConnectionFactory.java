3da3861a7f8b13da28f21ef7ae09afac53e8c713
Nana <fatalidade151@gmail.com>
2026-06-02 16:47:00 -0300

    Update ConnectionFactory.java

--- src/main/java/dao/generic/jdbc/ConnectionFactory.java
+++ src/main/java/dao/generic/jdbc/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     private static final String URL = "jdbc:postgresql://localhost:5432/vendasdb";
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "ave sangria0980";
+    private static final String SENHA = "";
 
     public static Connection getConnection() throws SQLException {
         try {
